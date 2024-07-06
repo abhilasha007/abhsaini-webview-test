b645e9f731be50840427490f9dc2daa296f32f7e
abhi <abhilashaasaini@gmail.com>
2024-07-07 01:11:18 +0530

    ++

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Web view Test
-        </p>
+        <h3>Test Web View</h3>
+        <WebView />
       </header>
+    </div>
+  );
+}
+
+function WebView() {
+  const [text, setText] = useState("");
+  const postMessageToNative = () => {
+    const webkit = window.webkit;
+
+    if (!webkit) {
+      setText("webkit is not available");
+    } else if (!webkit.messageHandlers) {
+      setText("webkit.messageHandlers is not available");
+    } else if (!webkit.messageHandlers.testHandler) {
+      setText("webkit.messageHandlers.testHandler is not available");
+    } else {
+      webkit.messageHandlers.testHandler.postMessage("Hello, world from JS!");
+      setText("message posted");
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={postMessageToNative} style={{ marginBottom: 20, paddingLeft: 20, paddingRight: 20 }}>
+        <h2>Post Message Native code</h2>
+      </button>
+      <div style={{ marginBottom: 20 }}>{text}</div>
     </div>
   );
 }
